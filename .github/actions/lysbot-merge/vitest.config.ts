3c71efe7e80225613a0e072b9f8f69cfe521d922
y <y@wxh.jp>
2025-12-09 13:18:10 +0900

    ci: add report func

--- .github/actions/lysbot-merge/vitest.config.ts
+++ .github/actions/lysbot-merge/vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     environment: 'node',
     include: ['src/**/*.test.ts'],
     coverage: {
-      reporter: ['text', 'json-summary', 'html'],
+      reporter: ['text', 'json-summary', 'html', 'lcov'],
       include: ['src/**/*.ts'],
       exclude: ['src/**/*.test.ts', 'src/main.ts'],
     },
