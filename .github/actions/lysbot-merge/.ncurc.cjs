6cf3a92df13d5d57cb04bf1453cb37ee18615132
y <y@wxh.jp>
2026-02-03 10:24:43 +0900

    fixup! ci(deps-lysbot-merge): upgrade dependencies

--- .github/actions/lysbot-merge/.ncurc.cjs
+++ .github/actions/lysbot-merge/.ncurc.cjs
@@ -1,4 +1,3 @@
 module.exports = {
-  target: (/** @type {string} */ name /*, semver */) =>
-    name === '@types/node' || name === '@actions/github' ? 'minor' : 'latest',
+  target: (/** @type {string} */ name /*, semver */) => (name === '@types/node' ? 'minor' : 'latest'),
 };
