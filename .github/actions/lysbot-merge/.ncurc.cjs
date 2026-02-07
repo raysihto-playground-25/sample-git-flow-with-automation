3d4de50ab7b570667320d8368e6e924c7d75a3b9
y <y@wxh.jp>
2026-02-08 01:47:03 +0900

    chore(lysbot-merge): optimize ncurc and npm upgrade scripts

--- .github/actions/lysbot-merge/.ncurc.cjs
+++ .github/actions/lysbot-merge/.ncurc.cjs
@@ -1,13 +1,3 @@
 module.exports = {
-  target: (/** @type {string} */ name /*, semver */) => {
-    if (
-      // Node: execution environment is Node 24; do not upgrade to Node 25+.
-      name === '@types/node' ||
-      // typescript-eslint: supported ESLint is ^8.57.0 || ^9.0.0 only. See https://typescript-eslint.io/users/dependency-versions/
-      name === 'eslint'
-    ) {
-      return 'minor';
-    }
-    return 'latest';
-  },
+  target: (/** @type {string} */ name /*, semver */) => (name === '@types/node' ? 'minor' : 'latest'),
 };
